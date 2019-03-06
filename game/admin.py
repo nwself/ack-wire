1917fd657578fc45785742bcdd5291503b099997
Nathan Self <nwself@cs.vt.edu>
2019-03-05 21:51:08 -0500

    Setup admin for models

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+@admin.register(models.Game)
+class GameAdmin(admin.ModelAdmin):
+    # list_display = ["id", "description"]
+    pass
+
+
+@admin.register(models.GameState)
+class GameStateAdmin(admin.ModelAdmin):
+    list_display = ["game", "effective"]
+    pass
