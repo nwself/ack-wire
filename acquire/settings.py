98320fadfa84276049178e1eb9876ef54ee867b2
Nathan Self <nathan@theselfs.org>
2019-12-18 16:28:04 -0500

    Get REDIS_URL from environment

--- acquire/settings.py
+++ acquire/settings.py
@@ -91,7 +91,8 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
+            "hosts": [(os.environ.get("REDIS_URL"), 6379)],
+            # "hosts": [('127.0.0.1', 6379)],
         },
     },
 }
