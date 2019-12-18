a4f3c5d3b396b1a25c313759bf86083045173c31
Nathan Self <nathan@theselfs.org>
2019-12-18 17:00:36 -0500

    Another commit in case heroku has the wrong info

--- acquire/settings.py
+++ acquire/settings.py
@@ -91,7 +91,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [(os.environ.get("REDIS_URL"), 6379)],
+            "hosts": [(os.environ.get("REDIS_URL"), 6379 )],
             # "hosts": [('127.0.0.1', 6379)],
         },
     },
